04bd3413f32974d3eb381fe92771adb87faccd13
Sean Williams <seananthonywilliams@gmail.com>
2014-06-14 21:32:57 -0300

    Completed part 3: parsing xml responses

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -65,11 +65,35 @@ class OracleOfBacon
       # your code here: 'elsif' clauses to handle other responses
       # for responses not matching the 3 basic types, the Response
       # object should have type 'unknown' and data 'unknown response'         
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      else
+        parse_unknown_response
       end
     end
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
+    end
+    def parse_graph_response
+      @type = :graph
+      @data = []
+      @doc.xpath('//link/descendant::*/text()').each do |node|
+        @data.push(node.content)
+      end
+    end
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = []
+      @doc.xpath('//spellcheck/descendant::*/text()').each do |node|
+        @data.push(node.content)
+      end
+    end
+    def parse_unknown_response
+      @type = :unknown
+      @data = 'unknown response type'
     end
   end
 end
